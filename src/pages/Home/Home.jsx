677549038832bf42c6556df911cdb3985a70ff0d
Nika Yakubova <lilit666an@gmail.com>
2020-12-18 21:50:34 +0300

    fix: Home.jsx add Paper for change theme; Add empty Message component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
+import { Paper } from '@material-ui/core'
 import React from 'react'
+import './Home.scss'
+
 
 export const Home = () => {
     return (
-        <div className='main'>
-            <h1>Hello world!</h1>
+        <div className='home'>
+            <Paper className='home__content' square={true}>
+                <h1>Hello world!</h1>
+            </Paper>
         </div>
     )
 }
