fcdb88f4ef8d37342166ea1f72c9adc5791aabed
Nika Yakubova <lilit666an@gmail.com>
2020-12-19 19:09:24 +0300

    Add: message styles(dark mode too) and props type for message

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,11 +1,56 @@
+import { Avatar, makeStyles, ThemeProvider, useTheme } from '@material-ui/core'
 import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { formatDistanceToNow } from 'date-fns'
 
-const Message = () => {
+import './Message.scss'
+import { ru } from 'date-fns/esm/locale'
+
+
+const useStyles = makeStyles({
+    bubble: theme => ({
+        backgroundColor:  theme.palette.primary.light,
+        color: theme.palette.primary.contrastText,
+        boxShadow: theme.shadows[1]
+    }),
+    date: theme => ({
+        color: theme.palette.text.hint
+    })
+
+})
+
+
+
+const Message = ({avatar, user = {}, text, date, isMe}) => {
+    const  theme  = useTheme()
+    const classes = useStyles(theme)
+    
     return (
-        <div className='message'>
-            
+        <div className={classNames('message', {'message--isme': isMe})}>
+            <Avatar 
+                className='message__avatar'  
+                src={avatar}
+                title={user.name}
+            />
+          <div className="message__content">
+              <div className={classNames('message__bubble', classes.bubble)} >
+                <p className="message__text">{text}</p>
+              </div>
+              <span className={classNames("message__date", classes.date)}>{formatDistanceToNow(date, {addSuffix: true, locale: ru })}</span>
+          </div>
         </div>
     )
 }
 
-export default Message
+
+Message.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    user: PropTypes.object.isRequired,
+    text: PropTypes.string.isRequired,
+    date: PropTypes.object.isRequired,
+    isMe: PropTypes.bool.isRequired
+
+}
+
+export default Message;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,36 @@
 import { Paper } from '@material-ui/core'
 import React from 'react'
+import { Message } from '../../components/Message'
+
+
+
 import './Home.scss'
+
+//for testing
+const date1 =  new Date('Sat Dec 19 2020 16:02:15 GMT+0300');
+const date2 = new Date('Sat Dec 19 2020 18:59:15 GMT+0300');
 
 
 export const Home = () => {
     return (
         <div className='home'>
             <Paper className='home__content' square={true}>
-                <h1>Hello world!</h1>
+                <Message 
+                    avatar='https://sun9-8.userapi.com/impg/aEsU4dOWknLGQ2VAEKlbYShAlx94gkJxK9YVnA/dJ7ZGpBcceg.jpg?size=1200x1200&quality=96&proxy=1&sign=9e737989500984f0595642511c323e93&type=album'
+                    text='Привет! Че как? 🌚 https://m.vk.com/mail?act=show&peer=158796696'
+                    date={date1}
+                    user={{name: 'Nika'}}
+                    isMe={true}
+                />
+
+                <Message 
+                    avatar='https://sun9-68.userapi.com/impf/apYlAwa-Ctdn0qtL0EbhimdVwhfpguyaxZO0OA/FZumFvWL0rM.jpg?size=810x1080&quality=96&sign=ee6b4246aa613f4a776027c90a2a7d76&type=album'
+                    text='Норм'
+                    date={date2}
+                    user={{name: 'Nika'}}
+                    isMe={false}
+                />
+                
             </Paper>
         </div>
     )
