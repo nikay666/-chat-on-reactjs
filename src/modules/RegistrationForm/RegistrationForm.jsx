9f7e97839b5de618841d5bd2ba8b4b7bc37ae2e4
Nika Yakubova <lilit666an@gmail.com>
2020-12-24 20:25:34 +0300

    Add: use Formik for registration  form

--- src/modules/RegistrationForm/RegistrationForm.jsx
+++ src/modules/RegistrationForm/RegistrationForm.jsx
@@ -6,10 +6,44 @@ import { Link } from 'react-router-dom'
 
 import { ButtonСustom } from '../../components/ButtonСustom'
 import { InputСustom } from '../../components/InputСustom'
+import { useFormik } from 'formik';
+
+
+const validate = values => {
+    const errors = {}
+
+    if(!values.name) {
+        errors.firstName = 'Required';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    if(!(values.password === values.passwordAgain)){
+        errors.passwordAgain = 'Passwords do not match'
+    }
+
+    return errors
+}
 
 
 const RegistrationForm = () => {
     const [success, setSuccess] = useState(false)
+    const  formik = useFormik({
+        initialValues: {
+            email: '',
+            name: '',
+            password: '',
+            passwordAgain: ''
+        },
+        validate,
+        onSubmit: values => {
+            console.log(JSON.stringify(values, null, 2))
+        }
+    })
 
     return (
         <>
@@ -21,7 +55,7 @@ const RegistrationForm = () => {
             </div>
 
             <Card className='auth__form  auth__form-registration' elevation={8}>
-                <CardContent component='form' >
+                <CardContent component='form' onSubmit={formik.handleSubmit}>
                     {
                         success ?  <>
                             <InfoIcon className='icon-auth' color="primary"  fontSize="large" />
@@ -32,33 +66,52 @@ const RegistrationForm = () => {
                                 required
                                 type='email'
                                 placeholder='E-mail' 
-                                label='Email'                                
+                                label='Email'  
+                                name="email"
+                                onChange={formik.handleChange}
+                                value={formik.values.email}   
+                                                      
                                 />
+                
                             <InputСustom  
                                 required
                                 type='text'
                                 placeholder='Ваше имя' 
                                 label='Ваше имя' 
+                                name="name"
+                                onChange={formik.handleChange}
+                                value={formik.values.name}     
                             />
                             <InputСustom  
                                 required
                                 type='password'
                                 placeholder='Пароль' 
                                 label='Пароль' 
+                                name="password"
+                                onChange={formik.handleChange}
+                                value={formik.values.password}     
                             />
                             <InputСustom  
                                 required
                                 type='password'
                                 placeholder='Повторить пароль' 
                                 label='Повторить пароль' 
+                                name="passwordAgain"
+                                onChange={formik.handleChange}
+                                value={formik.values.passwordAgain}  
+                                   
                             />
+
+                            {formik.touched.passwordAgain && formik.errors.passwordAgain ? (
+                                    <div>{formik.errors.passwordAgain}</div>
+                                ) : null}                          
                             <ButtonСustom 
                                 type='submit'
                                 color='primary'
                                 variant="contained"
                                 size='large'
                                 fullWidth={true}
-                                onClick={()  =>  setSuccess(!success)} 
+                           
                             >Зарегистрироваться</ButtonСustom>
                     
                         </>
